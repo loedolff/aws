ff948915064721e434e85eb6b4753ccef09cddc2
Hans Loedolff <hans_loedolff@cable.comcast.com>
2013-04-19 23:18:08 +0000

    adding ruby and ruby script to dowload from s3

--- pm/create-stacks.py
+++ pm/create-stacks.py
@@ -3,12 +3,15 @@ import random
 import string
 import time
 import re
+import os
+import traceback
+import sys
 
-# STACK_NAME='yjd'
-STACK_NAME= ''.join(random.sample(string.lowercase, 3))
+STACK_NAME='bbb'
+# STACK_NAME= ''.join(random.sample(string.lowercase, 3))
 
-# run a shell command
 def run_command(command):
+  """run a shell command"""
   p = subprocess.Popen(
     command,
     stdout=subprocess.PIPE,
@@ -20,13 +23,13 @@ def run_command(command):
     output.append(line)
   return "\n".join(output)
 
-# create puppet master stack
 def create_stack():
+  """execute a shell command to create a puppet master stack"""
   return run_command(
     ["cfn-create-stack " + STACK_NAME + " -f puppet-master.template"])
 
-# get puppet_master name
 def get_identifiers(stack_description):
+  """get puppet_master name"""
   global puppet_master
   global security_group
   m = re.match(r".*PuppetMasterPublicDNSName=(.*);PuppetClientSecurityGroup=(.*) .*", stack_description)
@@ -36,27 +39,33 @@ def get_identifiers(stack_description):
   else:
     raise Exception('Could not read Puppet Master or Security Group names', stack_description)
 
-# wait for completion
 def wait_for_completion():
-  for i in range(10):
+  """wait for stack creation to complete"""
+  for i in range(20):
     output = run_command(["cfn-describe-stacks " + STACK_NAME])
     if "CREATE_COMPLETE" in output:
       break
     time.sleep(10)
   get_identifiers(output)
 
-# test ssh
+def run_ssh_command(command):
+  """run an ssh command"""
+  return run_command('ssh -t -o "StrictHostKeyChecking no" -i ~/.ssh/mykeypair.pem ' + 
+      "ec2-user@" + puppet_master + ' "' + command + '"')
 
-def test_ssh():
-  for i in range(10):
-    output = run_command('ssh -t -o "StrictHostKeyChecking no" -i ~/.ssh/mykeypair.pem ' + 
-      "ec2-user@" + puppet_master + " ls -al")
+def wait_for_ssh():
+  """wait for SSH port to become available"""
+  output = run_ssh_command("echo")
+  for i in range(20):
     if 'Connection refused' not in output:
       break
-    time.sleep(5)
-  print '-----------------------'
+    time.sleep(10)
 
-# get security group from description
+def set_cfn_credentials():
+  """write the CFN credentials into .bashrc on remote host"""
+  run_ssh_command("echo EXPORT CFN_ACCESS_KEY=" + os.environ.get('CFN_ACCESS_KEY') + " >> .bashrc")
+  run_ssh_command("echo EXPORT CFN_SECRET_KEY=" + os.environ.get('CFN_SECRET_KEY') + " >> .bashrc")
+
 # start creating client stack
 
 # log into puppet master and install PM 3
@@ -68,13 +77,18 @@ def test_ssh():
 # point client to PM server
 # classify client
 # do puppet run on client
+
+# more todo:
+# would be nice to just write the instance name to a file rather than looking it up every time
+# would be nice to create a script to easily ssh to new instance.  hey, that's related to previous statement :-)
  
 try:
   create_stack()
   wait_for_completion()
   print "Puppet Master: ", puppet_master
   print "Security Group: ", security_group
-  test_ssh();
+  wait_for_ssh();
+  set_cfn_credentials();
 except Exception as instance:
-  print instance
+  print traceback.format_exc()
 
---
+++ pm/install-ruby.sh
@@ -0,0 +1,12 @@
+sudo yum -y install gem ruby-devel gcc rubygem-nokogiri
+??? sudo yum install -y libxml2 libxml2-devel libxslt libxslt-devel
+??? sudo yum install -y ruby ruby-libs ruby-mode ruby-rdoc ruby-irb ruby-ri ruby-docs 
+
+sudo yum install ruby19 rubygem-aws-sdk rubygem-json
+sudo yum install ruby19-devel
+
+export GEM_HOME=/usr/lib/ruby/gems/1.8
+
+sudo gem1.9 install nokogiri
+
+
