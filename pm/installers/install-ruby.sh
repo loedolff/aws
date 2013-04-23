028abc7e43267c9bf063d2e2f2d851b851d499e3
Hans Loedolff <hans@loedolff.com>
2013-04-22 23:07:34 -0700

    rename puppet master to master node.  add s3cmd installer

--- pm/installers/install-ruby.sh
+++
@@ -1,8 +0,0 @@
-
-sudo yum install -y ruby19 ruby19-devel 
-sudo yum install -y gcc libxml2 libxml2-devel libxslt libxslt-devel
-
-# gems are not installed using 'sude'.  they are installed in the user's home directory
-
-gem1.9 install --no-rdoc --no-ri nokogiri aws-sdk json
-
