4cf256d121261e4ee1a619cd1353542579384d11
Hans Loedolff <hans@loedolff.com>
2013-05-12 23:57:23 -0700

    more install script evolution

--- cf/builders/build-pdiff.sh
+++ cf/builders/build-pdiff.sh
@@ -1,15 +1,22 @@
 
 sudo yum -y install gcc-c++
 
-wget 'http://downloads.sourceforge.net/project/freeimage/Source%20Distribution/3.15.4/FreeImage3154.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Ffreeimage%2F' -O FreeImage.zip
-unzip FreeImage.zip
-
 wget http://www.cmake.org/files/v2.8/cmake-2.8.10.2.tar.gz
 tar xzf cmake-2.8.10.2.tar.gz 
 
 cd cmake-2.8.10.2
 ./configure
 gmake
+sudo gmake install 
 
+wget 'http://downloads.sourceforge.net/project/freeimage/Source%20Distribution/3.15.4/FreeImage3154.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Ffreeimage%2F' -O FreeImage.zip
+unzip FreeImage.zip
+cd FreeImage
+make
+sudo make install
 
+cd /home/ec2-user/dpxdt-1/pdiff
+cmake .
+make
+sudo make install
 
--- cf/installers/install-dpxdt.sh
+++ cf/installers/install-dpxdt.sh
@@ -1,4 +1,19 @@
 # Originally from https://www.djangoproject.com/download/
 
+sudo yum -y install git
+
 s3cmd get s3://hll-bootstrap/tars/Django-1.5.1.tar.gz  --force
 
+s3cmd get s3://hll-bootstrap/tars/dpxdt-1.zip --force
+s3cmd get s3://hll-bootstrap/tars/flask.zip --force
+s3cmd get s3://hll-bootstrap/tars/flask-wtf.zip --force
+s3cmd get s3://hll-bootstrap/tars/jinja2.zip --force
+s3cmd get s3://hll-bootstrap/tars/itsdangerous.zip --force
+s3cmd get s3://hll-bootstrap/tars/werkzeug.zip --force
+
+unzip flask.zip
+unzip flask-wtf.zip
+unzip jinja2.zip
+unzip itsdangerous.zip
+unzip werkzeug.zip
+unzip dpxdt-1.zip
